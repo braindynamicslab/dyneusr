59c718adeb726bf31c56509073278ec3e8e2c56e
Caleb W Geniesse <geniesse@stanford.edu>
2019-03-18 08:55:16 +0000

    setup.py edited online with Bitbucket

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='dyneusr',
     packages=find_packages(),
     version='0.1.0',
-    description='Dynamic Neural Spatiotemporal Representations.',
+    description='Dynamical Neural Spatiotemporal Representations.',
     author='calebgeniesse',
     license='BSD-3',
 )
