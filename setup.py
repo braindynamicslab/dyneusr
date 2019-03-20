59c37c1316cf6879a83a777fc1f145b6eede5ec9
calebgeniesse <calebgeniesse@gmail.com>
2019-03-19 18:10:08 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as f:
 # run setup
 setup(
     name='dyneusr',
-    version='0.2.2',
+    version='0.2.3',
     description='Dynamical Neural Spatiotemporal Representations.',
     long_description=long_description,
     long_description_content_type="text/markdown",	
